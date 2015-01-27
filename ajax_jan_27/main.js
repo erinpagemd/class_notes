2cf9c24b22e3a7e08f43c2246bc9a5cc923da0e5
YOUR NAME <erinpagemd@yahoo.com>
2015-01-27 10:19:15 -0600

    making a function that is a  new XMLHttpRequest

--- ajax_jan_27/main.js
+++ ajax_jan_27/main.js
@@ -19,4 +19,24 @@ request.open('GET', 'https://yspuku7qvh9u4cr3.firebaseio.com/.json');
 
 //asnch is default true. if you set to false, will wait for each request to return before continuing
 
+request.send();
+
+//it returns a string, so must parse
+
+var data = JSON.parse(request.response);
+
+//let's write a function that does this!!
+
+function getJSON(url){
+  var request = new XMLHttpRequest();
+  request.open('GET, url);
+  //b/c return will execute immediately instead of waiting for the response...             
+  request.onload = function (){
+    //we want data on the global scope
+    data = JSON.parse(request.response);
+  }
+  request.send();
+  return JSON.parse(request.response);
+}
+
 
